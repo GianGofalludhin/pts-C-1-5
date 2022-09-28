305355e78fbaf1ffd37298496a80a91bd13448de
GianGofalludhin <114574685+GianGofalludhin@users.noreply.github.com>
2022-09-28 17:06:18 +0700

    Add files via upload

---
+++ Gian gofalludhin_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+ cout << "Gian Gofalludhin"<< endl;
+    cout <<"kelas: 10 rpl 1"<< endl;
+    cout <<"nilai: 95"<< endl;
+    
+}
